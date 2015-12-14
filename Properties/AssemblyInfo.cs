a5816d64a2e776bf53f75e8d2628d36d165429a3
Thomas B Homburg <thomas@homburg.dk>
2015-12-14 11:31:22 +0100

    Convert to Windows Phone 8.1 project

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -15,14 +14,6 @@ using System.Resources;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("05fb10e2-111f-4063-bb83-2f20ce301582")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -30,9 +21,10 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("0.1.*")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: ComVisible(false)]
